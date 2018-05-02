7ebfeabd73233e46ae16ee82b94ecd5178165ff1
Sinan Sahin <sinan.sahin@rutgers.edu>
2018-05-02 15:59:45 -0400

    Fix

--- app/src/main/java/cs/rutgers/edu/android96/PhotoListAdapter.java
+++ app/src/main/java/cs/rutgers/edu/android96/PhotoListAdapter.java
@@ -1,11 +1,21 @@
 package cs.rutgers.edu.android96;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public abstract class PhotoListAdapter extends BaseAdapter {
 
-    ListView thumbnails;
+    Context context;
+    List<String> data;
+    private static LayoutInflater inflater = null;
+
+    private class photoItemView {
+        ImageView image;
+    }
 
 
 }
